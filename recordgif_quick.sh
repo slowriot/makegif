8ef3d00bedc1509697d7a85329864750d12e33e8
SlowRiot <slowriot@voxelstorm.com>
2017-12-10 16:40:13 +0000

    list the file we produced

--- recordgif_quick.sh
+++ recordgif_quick.sh
@@ -16,3 +16,4 @@ screensize_x=$(cut -d 'x' -f1 <<< "$screensize")
 screensize_y=$(cut -d '+' -f1 <<< "$screensize" | cut -d 'x' -f 2)
 
 byzanz-record "$outfile" -x $(((screensize_x / 2) - (size_x / 2))) -y $(((screensize_y / 2) - (size_y / 2))) -w $size_x -h $size_y -d $duration
+ls -alh "$outfile"
