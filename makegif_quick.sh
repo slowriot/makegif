d17984813e1397dc9c39d42adb65759e63c67aa8
SlowRiot <slowriot@voxelstorm.com>
2017-12-10 19:39:39 +0000

    handle filenames with spaces correctly

--- makegif_quick.sh
+++ makegif_quick.sh
@@ -6,7 +6,7 @@ fi
 
 tempdir="$(mktemp -d -p /dev/shm -t makegif-XXXXXX)"
 mkdir -p "$tempdir"
-cp $@ "$tempdir"
+cp "$@" "$tempdir"
 pushd "$tempdir"
 mogrify -monitor -limit thread $(nproc) -resize 512x512 *
 convert -limit thread 60 -monitor -delay 1x10 * out.gif
