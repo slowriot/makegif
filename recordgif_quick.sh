63fae11b7d3733a249c0fbf183e8e037b71fc12d
SlowRiot <slowriot@voxelstorm.com>
2024-12-09 19:23:23 +0000

    select a window to record dynamically

--- recordgif_quick.sh
+++ recordgif_quick.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#size_x=512
-#size_y=512
-size_x=1280
-size_y=720
 if [ -z "$1" ]; then
   duration=5
 else
@@ -11,9 +7,28 @@ else
 fi
 outfile="out.gif"
 
-screensize=$(xwininfo -root | awk '/-geo/{print $2}')
-screensize_x=$(cut -d 'x' -f1 <<< "$screensize")
-screensize_y=$(cut -d '+' -f1 <<< "$screensize" | cut -d 'x' -f 2)
+echo "Select the window to record"
 
-byzanz-record "$outfile" -x $(((screensize_x / 2) - (size_x / 2))) -y $(((screensize_y / 2) - (size_y / 2))) -w $size_x -h $size_y -d $duration
-ls -alh "$outfile"
+window_geometry=$(xdotool selectwindow getwindowgeometry --shell)
+pos_x=$(grep "^X=" <<< "$window_geometry" | cut -d '=' -f 2-)
+pos_y=$(grep "^Y=" <<< "$window_geometry" | cut -d '=' -f 2-)
+size_x=$(grep "^WIDTH=" <<< "$window_geometry" | cut -d '=' -f 2-)
+size_y=$(grep "^HEIGHT=" <<< "$window_geometry" | cut -d '=' -f 2-)
+echo "Coords: $pos_x x $pos_y, size: $size_x x $size_y"
+
+echo -n "Recording in 3..."
+sleep 1;
+echo -ne "\rRecording in 2 .."
+sleep 1;
+echo -ne "\rRecording in 1  ."
+sleep 1;
+echo -e "\rRecording for $duration seconds"
+
+#screensize=$(xwininfo -root | awk '/-geo/{print $2}')
+#screensize_x=$(cut -d 'x' -f1 <<< "$screensize")
+#screensize_y=$(cut -d '+' -f1 <<< "$screensize" | cut -d 'x' -f 2)
+
+byzanz-record "$outfile" -x "$pos_x" -y "$pos_y" -w "$size_x" -h "$size_y" -d "$duration"
+
+echo "Recording saved to $outfile"
+exit
